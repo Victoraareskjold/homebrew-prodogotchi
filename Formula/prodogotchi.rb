05b02cc38b799f96d5d5e1e23649b47ea60f5d3c
Victor Aareskjold <victor.aaareskjold@gmail.com>
2025-06-08 13:41:10 +0200

    feat: added complete

--- Formula/prodogotchi.rb
+++ Formula/prodogotchi.rb
@@ -1,8 +1,8 @@
 class Prodogotchi < Formula
     desc "CLI tool for managing tasks with fun"
     homepage "https://github.com/victoraareskjold/prodogotchi"
-    url "https://github.com/victoraareskjold/prodogotchi/releases/download/v0.1.1/prodogotchi"
-    sha256 "02c64cf335dc61787e94fa800c3147ce9cbc87e05b0bec9ad02cee356fe0652d"
+    url "https://github.com/victoraareskjold/prodogotchi/releases/download/v0.2.0/prodogotchi"
+    sha256 "2253b8b2ec255572ddfc96ba962d1dae0d8749927f6960f42befa5ade27397ac"
     license "MIT"
   
     def install
